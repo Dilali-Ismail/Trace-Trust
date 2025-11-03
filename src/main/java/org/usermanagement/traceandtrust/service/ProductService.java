74e13bfc2a34cdb559cb0a59016d444630841967
Dilaly <ismaildilali@gmail.com>
2025-11-03 13:49:56 +0100

    admin can get all products

--- src/main/java/org/usermanagement/traceandtrust/service/ProductService.java
+++ src/main/java/org/usermanagement/traceandtrust/service/ProductService.java
@@ -3,8 +3,11 @@ package org.usermanagement.traceandtrust.service;
 import org.usermanagement.traceandtrust.dto.CreateProductRequest;
 import org.usermanagement.traceandtrust.dto.ProductDto;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductService {
     ProductDto createProduct(CreateProductRequest request, UUID actorId);
+    List<ProductDto> getAllProducts(UUID actorId);
+    ProductDto getProductById(UUID productId, UUID actorId);
 }
