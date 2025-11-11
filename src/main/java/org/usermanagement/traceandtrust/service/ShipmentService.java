ce130af6b47d6a254ef336bc76d05d62adf75ec2
Dilaly <ismaildilali@gmail.com>
2025-11-11 21:22:26 +0100

    TRUS-16 <message>

--- src/main/java/org/usermanagement/traceandtrust/service/ShipmentService.java
+++ src/main/java/org/usermanagement/traceandtrust/service/ShipmentService.java
@@ -3,9 +3,12 @@ package org.usermanagement.traceandtrust.service;
 import org.usermanagement.traceandtrust.dto.CreateShipmentRequest;
 import org.usermanagement.traceandtrust.dto.ShipmentDto;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ShipmentService {
 
     ShipmentDto createShipment(CreateShipmentRequest request, UUID actorId);
+    ShipmentDto dispatchShipment(UUID shipmentId, UUID actorId);
+    List<ShipmentDto> getAllShipments(UUID actorId);
 }
